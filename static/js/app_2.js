2c872b60a18ae1e6542ffed84481c7aac18c6032
emilyfrels <emily.n.frels@gmail.com>
2020-10-20 08:18:26 -0500

    updated app.py for flask, updated CSS, made some formatting changes, started on legend

---
+++ static/js/app_2.js
@@ -0,0 +1,126 @@
+// console.log because we can
+console.log("app_2.js");
+
+// create variable for course data and map
+var courseData;
+var myMap;
+
+
+// create function to initialize the map
+function initMap() {
+    myMap = L.map("map", {
+        center: [46.392410, -94.636230],
+        zoom: 6.5,
+      });
+    console.log("Created map object.");
+
+    // create tile layer
+    L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        tileSize: 512,
+        maxZoom: 18,
+        zoomOffset: -1,
+        id: "mapbox/streets-v11",
+        accessToken: API_KEY
+    }).addTo(myMap);
+    
+    console.log("Added tile layer");
+}
+
+// create function to read in course data
+function getCourseData() {
+    d3.csv("/results/course_subset.csv").then(function(data) {
+        courseData = data;
+        addMarkers();
+    });
+}
+
+// create function to add markers
+function addMarkers() {
+    courseData.forEach(function(c) {
+        var marker = L.circleMarker([+c.lat, +c.lng]);
+        var public = c.public_private === 'Public';
+        var private = c.public_private === 'Private';
+        var semiPrivate = c.public_private === 'Semi-Private';
+        var resort = c.public_private === 'Resort';
+        var military = c.public_private === 'Military';
+
+        if(public) {
+            marker.setStyle({
+                radius: 8,
+                fillColor: '#7FFF00',
+                fillOpacity: 1,
+                color: '#000000',
+            });
+        } 
+
+        else if(private) {
+            marker.setStyle({
+                radius: 8,
+                fillColor: '#DC143C',
+                fillOpacity: 1,
+                color: '#000000',
+            });
+        }
+
+        else if(semiPrivate) {
+            marker.setStyle({
+                radius: 8,
+                fillColor: '#5F9EA0',
+                fillOpacity: 1,
+                color: '#000000',
+            });
+        }
+
+        else if(resort) {
+            marker.setStyle({
+                radius: 8,
+                fillColor: '#00FFFF',
+                fillOpacity: 1,
+                color: '#000000',
+            });
+        }
+
+        else if(military) {
+            marker.setStyle({
+                radius: 8,
+                fillColor: '#000000',
+                fillOpacity: 1,
+                color: '#000000',
+            });
+        }
+        
+        else{
+        marker.setStyle({
+            radius: 8,
+            fillColor: '#F8F8FF',
+            color: '#000000',
+            fillOpacity: 1
+        });
+        }
+
+        marker.addTo(myMap);
+        
+        // add popup to selected marker
+        marker.bindPopup("<h6>" + c.course + "</h6> <hr> <p><strong>Address: </strong></br>" + c.street + "<br>" + c.city + ", " + c.state + " " + c.zip_code + "</p><p><strong>Access: </strong>" + c.public_private + "</br><strong>Holes: </strong>" + c.hole + "<p>")
+        .addTo(myMap);
+
+
+        // add legend
+        var legend = L.control({
+            position: 'topright'
+        });
+
+        legend.onAdd = function() {
+            var div = L.DomUtil.create('div', 'legend');
+            return div;
+        }
+
+        legend.addTo(myMap);
+    })
+}
+
+
+// call functions
+initMap();
+getCourseData();
